4fad66478863655a23f864260012698434c54b7c
James Muscat <james.muscat@alces-software.com>
2018-02-22 15:40:33 +0000

    Check for `config.yml` before allowing clusterable to install.
    Also make sure we run `initialize` before `start`.

--- clusterable/package/install.sh
+++ clusterable/package/install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+set -e
+
+if [ ! -f "${cw_ROOT}/etc/config.yml" ]; then
+  >&2 echo "${cw_ROOT}/etc/config.yml not found, but is required for clusterable-events-aws to function."
+  exit 1
+fi
+
 require files
 files_load_config distro
 
@@ -18,5 +26,6 @@ chmod 0700 "${cw_ROOT}"/libexec/share/nologin-control
 mkdir -p "${cw_ROOT}"/var/lib/event-periodic/scripts
 
 "${cw_ROOT}"/etc/handlers/00-clusterable/preconfigure >> /var/log/clusterware/clusterable.log
+"${cw_ROOT}"/etc/handlers/00-clusterable/initialize >> /var/log/clusterware/clusterable.log
 "${cw_ROOT}"/etc/handlers/00-clusterable/start >> /var/log/clusterware/clusterable.log
 "${cw_ROOT}"/etc/handlers/00-clusterable/node-started >> /var/log/clusterware/clusterable.log
