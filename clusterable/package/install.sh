a6bde6b66c9cf0fef96da1431e9924f664cc8cbe
James Muscat <james.muscat@alces-software.com>
2018-03-08 14:56:16 +0000

    Allow `clusterable-events-aws` to be installed without a `config.yml`.
    When we're building a base image, we want this package to be installed
    but only configured once the "real" machine boots from the image.

--- clusterable/package/install.sh
+++ clusterable/package/install.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-if [ ! -f "${cw_ROOT}/etc/config.yml" ]; then
-  >&2 echo "${cw_ROOT}/etc/config.yml not found, but is required for clusterable-events-aws to function."
-  exit 1
-fi
-
 cp -R data/* "${cw_ROOT}"
 
 chmod 0700 "${cw_ROOT}"/libexec/share/clusterware-key-manager
 chmod 0700 "${cw_ROOT}"/libexec/share/nologin-control
 mkdir -p "${cw_ROOT}"/var/lib/event-periodic/scripts
 
-"${cw_ROOT}"/etc/handlers/00-clusterable/preconfigure >> /var/log/clusterware/clusterable.log
-"${cw_ROOT}"/etc/handlers/00-clusterable/initialize >> /var/log/clusterware/clusterable.log
-"${cw_ROOT}"/etc/handlers/00-clusterable/start >> /var/log/clusterware/clusterable.log
-"${cw_ROOT}"/etc/handlers/00-clusterable/node-started >> /var/log/clusterware/clusterable.log
+if [ ! -f "${cw_ROOT}/etc/config.yml" ]; then
+  # This is probably because we're making an image and we won't provide userdata until it boots properly
+  echo "${cw_ROOT}/etc/config.yml not found, but is required for clusterable-events-aws to function. Postponing configuration until reboot."
+else
+  "${cw_ROOT}"/etc/handlers/00-clusterable/preconfigure >> /var/log/clusterware/clusterable.log
+  "${cw_ROOT}"/etc/handlers/00-clusterable/initialize >> /var/log/clusterware/clusterable.log
+  "${cw_ROOT}"/etc/handlers/00-clusterable/start >> /var/log/clusterware/clusterable.log
+  "${cw_ROOT}"/etc/handlers/00-clusterable/node-started >> /var/log/clusterware/clusterable.log
+fi
