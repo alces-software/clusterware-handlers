a507b4feb7aaa39e4afc9809704f3418f98be823
James Muscat <james.muscat@alces-software.com>
2018-03-16 12:13:43 +0000

    `member_each` is marginally awkward to use...

--- cluster-slurm/package/install.sh
+++ cluster-slurm/package/install.sh
@@ -4,9 +4,14 @@ require member
 
 cp -R data/* "${cw_ROOT}"
 
+_handle_members() {
+  shift  # Gets rid of the '--' that member_each starts with
+  echo "$@" | "${cw_ROOT}/etc/handlers/cluster-slurm/member-join"
+}
+
 if [ ! -d "${cw_ROOT}/etc/config/cluster" ]; then
   echo "Cluster not yet configured. Deferring Slurm configuration until next boot."
 else
   "${cw_ROOT}/etc/handlers/cluster-slurm/configure"
-  member_each "${cw_ROOT}/etc/handlers/cluster-slurm/member-join"
+  member_each _handle_members
 fi
