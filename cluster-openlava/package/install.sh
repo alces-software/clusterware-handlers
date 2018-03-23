432575af0b1a0da8acc5a5fb4bd3841065d2452b
James Muscat <james.muscat@alces-software.com>
2018-03-23 17:38:04 +0000

    Perform OpenLava configuration if appropriate.

--- cluster-openlava/package/install.sh
+++ cluster-openlava/package/install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 cp -R data/* "${cw_ROOT}"
+
+_handle_members() {
+  shift  # Gets rid of the '--' that member_each starts with
+  echo "$@" | "${cw_ROOT}/etc/handlers/cluster-openlava/member-join"
+}
+
+if [ ! -d "${cw_ROOT}/etc/config/cluster" ]; then
+  echo "Cluster not yet configured. Deferring OpenLava configuration until next boot."
+else
+  "${cw_ROOT}/etc/handlers/cluster-openlava/configure"
+  member_each _handle_members
+fi
