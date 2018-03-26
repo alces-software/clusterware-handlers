6a2ac3204d3c2faf3ed068bb3d26a6af2ad7d8b4
James Muscat <james.muscat@alces-software.com>
2018-03-26 11:37:50 +0100

    Call `start` handler too.

--- cluster-openlava/package/install.sh
+++ cluster-openlava/package/install.sh
@@ -11,5 +11,6 @@ if [ ! -d "${cw_ROOT}/etc/config/cluster" ]; then
   echo "Cluster not yet configured. Deferring OpenLava configuration until next boot."
 else
   "${cw_ROOT}/etc/handlers/cluster-openlava/configure"
+  "${cw_ROOT}/etc/handlers/cluster-openlava/start"
   member_each _handle_members
 fi
--- cluster-pbspro/package/install.sh
+++ cluster-pbspro/package/install.sh
@@ -11,5 +11,6 @@ if [ ! -d "${cw_ROOT}/etc/config/cluster" ]; then
   echo "Cluster not yet configured. Deferring PBS Pro configuration until next boot."
 else
   "${cw_ROOT}/etc/handlers/cluster-pbspro/configure"
+  "${cw_ROOT}/etc/handlers/cluster-pbspro/start"
   member_each _handle_members
 fi
